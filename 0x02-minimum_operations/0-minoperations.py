991a4468503fee5150d2ec1e90fcd3c7bca730f7
Gug028 <gramthembu@gmail.com>
2024-07-11 16:13:06 +0200

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to result in exactly n H
+    characters in the file.
+
+    Parameters:
+    - n (int): The target number of H characters.
+
+    Returns:
+    - int: The fewest number of operations needed to achieve exactly n H
+           characters in the file.
+    Returns 0 if n is impossible to achieve.
+    """
+    if n <= 1:
+        return 0
+
+    min_ops = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            min_ops += factor
+            n //= factor
+        factor += 1
+
+    return min_ops
