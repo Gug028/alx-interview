c9519fe25ca21ae1b5477b6fb8f0914cb919a08d
Gug028 <gramthembu@gmail.com>
2024-08-21 12:58:48 +0200

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+The script determines the fewest number of coins needed to meet a
+given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given total.
+
+    Args:
+    coins (list of int): List of coin values.
+    total (int): The total amount to be met.
+
+    Returns:
+    int: Fewest number of coins needed to meet total, or -1 if not
+    possible.
+    """
+    if total <= 0:
+        return 0
+
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for e in coin:
+            while (total >= e):
+                counter += 1
+                total -= e
+        if total == 0:
+            return counter
+        return -1
